2e0ee3ed69f2729205f680aa2b160c7c030a3686
Ania Lothaire <anialothaire@gmail.com>
2024-01-19 14:12:55 +0100

    Updated imageloader to skip GIF loading

--- xivmodimage/ImageLoader.cs
+++ xivmodimage/ImageLoader.cs
@@ -8,16 +8,29 @@
         {
             this.logMessageCallback = logMessageCallback;
         }
+
         public async Task<Image> LoadImageAsync(string imageUrl)
         {
             using (var httpClient = new HttpClient())
             {
                 var response = await httpClient.GetAsync(imageUrl);
+
                 if (response.IsSuccessStatusCode)
                 {
-                    using (var stream = await response.Content.ReadAsStreamAsync())
+                    var contentType = response.Content.Headers.ContentType?.MediaType;
+
+                    // Check if the content type is not GIF
+                    if (contentType != null && !contentType.Equals("image/gif", StringComparison.OrdinalIgnoreCase))
                     {
-                        return Image.FromStream(stream);
+                        using (var stream = await response.Content.ReadAsStreamAsync())
+                        {
+                            return Image.FromStream(stream);
+                        }
+                    }
+                    else
+                    {
+                        logMessageCallback("Skipping GIF image loading to prevent crashes.");
+                        return null;
                     }
                 }
                 else
